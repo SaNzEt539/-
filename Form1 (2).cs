cbd1f8c4dbc2ba47993192fe5021869b1ab89192
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-01 19:50:33 +0300

    Add files via upload

---
+++ Form1 (2).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+
+            if ((x * x + y * y < 144) && y < x)
+            {
+                textBox3.Text = Convert.ToString("Да");
+            }
+
+            else if ((x * x + y * y == 144) || y == x)
+            {
+                textBox3.Text = Convert.ToString("На границе");
+            }
+
+            else
+            {
+                textBox3.Text = Convert.ToString("Нет");
+            }
+        }
+    }
+}
---
+++ Form1 (3).cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form_2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            num = Convert.ToInt32(textBox1.Text);
+                if (num < 0)
+                {
+                textBox2.Text = Convert.ToString("Вы ошиблись");
+                }
+                else if (num < 1)
+                {
+                textBox2.Text = Convert.ToString("Младенец");
+                }
+                else if (1 < num && num < 11)
+                {
+                textBox2.Text = Convert.ToString("Ребёнок");
+                }
+                else if (12 < num && num < 15)
+                {
+                textBox2.Text = Convert.ToString("Подросток");
+                }
+                else if (16 < num && num < 25)
+                {
+                textBox2.Text = Convert.ToString("Юноша");
+                }
+                else if (26 < num && num < 70)
+                {
+                textBox2.Text = Convert.ToString("Мужчина");
+                }
+                else if (num > 70)
+                {
+                textBox2.Text = Convert.ToString("Пожилой человек");
+                }
+            else
+            {
+                textBox2.Text = Convert.ToString("Вы ошиблись");
+            }
+        }
+    }
+}

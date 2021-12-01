1c7734fa26b7f5b8a5f2b12046bb32a12c68b53a
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-01 19:20:14 +0300

    Add files via upload

---
+++ Form1 (1).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            int a = n / 100;
+            int c = n % 10;
+            int b = n % 100 / 10;
+            if (a == b && b == c)
+            {
+                textBox2.Text = ("Цифры трехзначного числа одинаковые");
+            }
+            else
+            {
+                textBox2.Text = ("Цифры трехзначного числа не одинаковые");
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r1, r2;
+
+            r1 = Convert.ToDouble(textBox1.Text);
+
+            r2 = Convert.ToDouble(textBox2.Text);
+
+            double s = Math.PI * (r2 * r2 - r1 * r1);
+
+            textBox3.Text = Convert.ToString(s);
+        }
+    }
+}
